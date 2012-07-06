f05106af0efb9696c8ecd9b5ac8d164d109aa2c8
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-07-06 14:54:32 -0700

    tenderloin: build omx from source
    Change-Id: I6183b54d987efd481a4ffc5c03b6080165dd33a9
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

--- tenderloin-vendor-blobs.mk
+++ tenderloin-vendor-blobs.mk
@@ -31,9 +31,6 @@ PRODUCT_COPY_FILES += \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 PRODUCT_COPY_FILES += \
-    vendor/hp/tenderloin/proprietary/video/mm-video-driver-test:system/bin/mm-video-driver-test\
-    vendor/hp/tenderloin/proprietary/video/mm-vdec-omx-test:system/bin/mm-vdec-omx-test \
-    vendor/hp/tenderloin/proprietary/video/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
     vendor/hp/tenderloin/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
     vendor/hp/tenderloin/proprietary/lib/libgemini.so:system/lib/libgemini.so \
     vendor/hp/tenderloin/proprietary/lib/libqmi.so:system/lib/libqmi.so \
